0191f37b6d58ecd291d750ea4dd658b06ad268fe
RgregoR <gredelonghi@gmail.com>
2024-09-05 22:35:41 +0200

    update 20240905_2235_jbe

--- rst_RUST_BOOK_PROJECTS/methods/src/main.rs
+++ rst_RUST_BOOK_PROJECTS/methods/src/main.rs
@@ -10,10 +10,21 @@ struct Rectangle {
 	height: u32,
 }
 
-/* method */
+/* methods */
 impl Rectangle {
+	/* method area */
 	fn area(&self) -> u32 {
 		self.width * self.height
+	}
+
+	/* method can_hold */
+	fn can_hold(&self, other: &Rectangle) -> bool {
+		self.width > other.width && self.height > other.height
+	}
+
+	/* method that returns WxH string */
+	fn hxv(&self) -> String {
+		return format!("{}x{}", self.width, self.height);
 	}
 }
 
@@ -25,8 +36,36 @@ fn main() {
 		height: 50,
 	};
 
+	let rect2 = Rectangle {
+		width:  10,
+		height: 40,
+	};
+
+	let rect3 = Rectangle {
+		width:  60,
+		height: 45,
+	};
+
 	println!(
 		"The area of the rectangle is {} sqauare pixels.",
 		rect1.area()
 	);
-}
+	
+	println!("---");
+
+	println!(
+		"Can rect1({}) hold rect2({})? {}",
+		rect1.hxv(),
+		rect2.hxv(),
+		rect1.can_hold(&rect2)
+	);
+
+	println!(
+		"Can rect1({}) hold rect3({})? {}",
+		rect1.hxv(),
+		rect3.hxv(),
+		rect1.can_hold(&rect3)
+	);
+
+} /* end main */
+
