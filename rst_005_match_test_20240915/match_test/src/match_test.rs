73a9ec0b8ec8ccdf36a787d90cff4efb744f358e
RgregoR <gredelonghi@gmail.com>
2024-09-16 08:26:00 +0200

    update 20240916_0825_jbe

--- rst_005_match_test_20240915/match_test/src/match_test.rs
+++ rst_005_match_test_20240915/match_test/src/match_test.rs
@@ -1,5 +1,15 @@
 pub fn age_test(age: &u32) {
-	/*
+
+	/* BETTER */
+	match age {
+		0  ..   7 => println!("An infant."),
+		7  ..= 17 => println!("Not yet grownup."),
+		18 ..= 65 => println!("An adoult, but not yet senior."),
+		61 ..  86 => println!("Senior. Enjoy the rest of your life."),
+		86 ..     => println!("You're still alive? Welcome to the Dinosaurs' club!"),
+	}
+
+	/* IF ACTIONS IN CODE BLOCKS ...
 	match age {
 		0 ..= 17 => {
 			println!("Not yet grownup.");
@@ -18,13 +28,4 @@ pub fn age_test(age: &u32) {
 		}
 	}
 	*/
-
-	/* BETTER */
-	match age {
-		0  ..   7 => println!("An infant."),
-		7  ..= 17 => println!("Not yet grownup."),
-		18 ..= 65 => println!("An adoult, but not yet senior."),
-		61 ..  86 => println!("Senior. Enjoy the rest of your life."),
-		86 ..     => println!("You're still alive? Welcome to the Dinosaurs' club!"),
-	}
 }
