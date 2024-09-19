d9bd66e11975fa8e1b3dc867a94d4de2d1c82fc7
r-gregor <gredelonghi@gmail.com>
2024-09-19 09:10:30 +0200

    update 20240919_0910_en

--- rst_006_vec_test_20240916/vec_test/src/main.rs
+++ rst_006_vec_test_20240916/vec_test/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let mut v1: Vec<i32> = Vec::new();
-    
+
     println!("{:>5} | {:>5} | {:>5}", "#", "size", "cap");
     println!("---------------------");
     for i in 1..10001 {
-        v1.push(1);
+        v1.push(i);
         println!("{:5} | {:5} | {:5}", i, v1.len(), v1.capacity());
     }
 }
